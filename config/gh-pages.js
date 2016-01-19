17cd0e34ebd2cc6d39b86c087fc110e088f00251
Naohiro Hosokawa <sarumokikaradive1980@gmail.com>
2016-01-20 00:06:28 +0900

    hoso-20160120-01

---
+++ config/gh-pages.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  module.exports = {
+    options: {
+      base: 'dist',
+      repo: 'https://github.com/Naohiro1980/Lifebooks.git'
+    },
+    src: ['**']
+  };
+})();
